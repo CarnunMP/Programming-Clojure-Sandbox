13bc16e884978321f559ae8101864d62e0834bcb
CarnunMP <carnun@hotmail.co.uk>
2020-07-14 23:31:19 +0100

    Some more examples from 3.2

--- 3-unifying-data-with-sequences.clj
+++ 3-unifying-data-with-sequences.clj
@@ -3,3 +3,11 @@
 
 ; 'take' is one way to view a finite subject of an infinite, lazy sequence:
 (take 10 (whole-numbers)) ; => (1 2 3 4 5 6 7 8 9 10)
+
+; 'interleave' runs until one of the collections passed to it runs out of items:
+(interleave (whole-numbers) ["A" "B" "C" "D" "E"]) ; => (1 "A" 2 "B" 3 "C" 4 "D" 5 "E")
+
+; One interesting way to filter a sequence is with 'take-while', which takes while a predicate remains true:
+(take-while (complement #{\a\e\i\o\u}) "the-quick-brown-fox") ; => (\t \h)
+; Note: the set #{\a\e\i\o\u} is acting as a predicate function, here — and 'complement' is reversing its behavior!
+; So, it returns true when its argument _isn't_ a vowel.
